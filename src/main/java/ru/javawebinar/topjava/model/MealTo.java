e6382df9d8051ead2bda14d850e957e1e9b2c39d
dd.darenko <dd.darenko@jet.su>
2025-02-10 08:57:46 +0300

    Completing homework - Without Optional
    A servlet has been developed that provides a list of products in the form of a table.

--- src/main/java/ru/javawebinar/topjava/model/MealTo.java
+++ src/main/java/ru/javawebinar/topjava/model/MealTo.java
@@ -4,20 +4,36 @@ import java.time.LocalDateTime;
 
 public class MealTo {
     private final LocalDateTime dateTime;
-
+    
     private final String description;
-
+    
     private final int calories;
-
+    
     private final boolean excess;
-
+    
     public MealTo(LocalDateTime dateTime, String description, int calories, boolean excess) {
         this.dateTime = dateTime;
         this.description = description;
         this.calories = calories;
         this.excess = excess;
     }
-
+    
+    public boolean getExcess() {
+        return excess;
+    }
+    
+    public String getDescription() {
+        return description;
+    }
+    
+    public int getCalories() {
+        return calories;
+    }
+    
+    public LocalDateTime getDateTime() {
+        return dateTime;
+    }
+    
     @Override
     public String toString() {
         return "MealTo{" +
