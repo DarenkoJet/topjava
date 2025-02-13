e330f6179e7779b8a2de8344a58cb2bc33bea177
dd.darenko <dd.darenko@jet.su>
2025-02-13 07:38:44 +0300

    FIX - thread-safe processing

--- src/main/java/ru/javawebinar/topjava/service/CRUD/StorageMemoryMealsService.java
+++ src/main/java/ru/javawebinar/topjava/service/CRUD/StorageMemoryMealsService.java
@@ -4,10 +4,11 @@ import org.slf4j.Logger;
 import ru.javawebinar.topjava.model.Meal;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
@@ -16,70 +17,42 @@ import static org.slf4j.LoggerFactory.getLogger;
 public class StorageMemoryMealsService implements MealsService {
     private static final Logger log = getLogger(StorageMemoryMealsService.class);
     
-    private final Map<Integer, Meal> meals = new HashMap<>();
+    private final ConcurrentMap<Integer, Meal> meals = new ConcurrentHashMap<>();
     
     private final Lock lock = new ReentrantLock();
     
-    private int counter = 1;
+    private final AtomicInteger counter = new AtomicInteger(1);
     
     @Override
-    public Meal add(Meal mealNew) {
+    public synchronized Meal add(Meal mealNew) {
         Meal meal = new Meal(mealNew.getDateTime(), mealNew.getDescription(), mealNew.getCalories());
-        try {
-            if (lock.tryLock(1, TimeUnit.SECONDS)) {
-                meal.setId(counter);
-                meals.put(counter, meal);
-                counter++;
-            }
-        } catch (InterruptedException e) {
-            log.error(e.getMessage());
-            return null;
-        } finally {
-            lock.unlock();
-        }
+        meal.setId(counter.get());
+        meals.put(counter.get(), meal);
+        counter.addAndGet(1);
         return meal;
     }
     
     @Override
-    public List<Meal> getAll() {
+    public synchronized List<Meal> getAll() {
         return new ArrayList<>(meals.values());
     }
     
     @Override
-    public Meal update(Meal updatedMeal) {
-        Meal meal = null;
-        try {
-            if (lock.tryLock(1, TimeUnit.SECONDS)) {
-                meal = meals.get(updatedMeal.getId());
-                meal.setDescription(updatedMeal.getDescription());
-                meal.setDateTime(updatedMeal.getDateTime());
-                meal.setCalories(updatedMeal.getCalories());
-                meals.put(updatedMeal.getId(), meal);
-            }
-        } catch (InterruptedException e) {
-            log.error(e.getMessage());
-            return null;
-        } finally {
-            lock.unlock();
-        }
+    public synchronized Meal update(Meal updatedMeal) {
+        Meal meal = meals.get(updatedMeal.getId());
+        meal.setDescription(updatedMeal.getDescription());
+        meal.setDateTime(updatedMeal.getDateTime());
+        meal.setCalories(updatedMeal.getCalories());
         return meal;
     }
     
     @Override
-    public void delete(int id) {
-        try {
-            if (lock.tryLock(1, TimeUnit.SECONDS)) {
-                meals.remove(id);
-            }
-        } catch (InterruptedException e) {
-            log.error(e.getMessage());
-        } finally {
-            lock.unlock();
-        }
+    public synchronized void delete(int id) {
+        meals.remove(id);
     }
     
     @Override
-    public Meal get(int id) {
+    public synchronized Meal get(int id) {
         return meals.get(id);
     }
 }
